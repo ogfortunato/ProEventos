834fec3afa24fff7cea238ae3387d792da241dca
Nikopol <ottatec@gmail.com>
2022-02-28 21:46:30 -0300

    Component Título

--- Front/ProEventos-App/src/app/shared/titulo/titulo.component.ts
+++ Front/ProEventos-App/src/app/shared/titulo/titulo.component.ts
@@ -6,8 +6,10 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./titulo.component.scss']
 })
 export class TituloComponent implements OnInit {
-  @Input()
-  titulo!: string;
+  @Input() titulo!: string;
+  @Input() iconClass = 'fa fa-user';
+  @Input() subtitulo = 'Desde 2021';
+  @Input() botaoListar = false;
   constructor() { }
 
   ngOnInit(): void {
