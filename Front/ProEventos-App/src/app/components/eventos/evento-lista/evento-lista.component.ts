0b18e7d98d19f78da0d0c8de6afd38b2c0136ab4
Nikopol <ottatec@gmail.com>
2022-03-01 18:29:03 -0300

    RedirectTo and Rotas Detalhes

--- Front/ProEventos-App/src/app/components/eventos/evento-lista/evento-lista.component.ts
+++ Front/ProEventos-App/src/app/components/eventos/evento-lista/evento-lista.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
+import { Router } from '@angular/router';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { ToastrService } from 'ngx-toastr';
 import { Evento } from 'src/app/models/Evento';
 import { EventoService } from 'src/app/services/evento.service';
-
 
 @Component({
   selector: 'app-evento-lista',
@@ -43,7 +43,8 @@ export class EventoListaComponent implements OnInit {
     private eventoService: EventoService,
     private modalService: BsModalService,
     private toastr: ToastrService,
-    private spinner: NgxSpinnerService
+    private spinner: NgxSpinnerService,
+    private router: Router
     ) { }
 
   public ngOnInit(): void {
@@ -86,6 +87,10 @@ export class EventoListaComponent implements OnInit {
    
     public decline(): void {
       this.modalRef?.hide();
+    }
+
+    detalheEvento(id: number): void {
+      this.router.navigate([`eventos/detalhe/${id}`]);
     }
 
 }
